9aed50021450f96f710339e0bdc102413ac7d983
pandongwei <pandongwei123@gmail.com>
2024-09-13 16:01:41 +0800

    Initial commit

--- static/js/index.js
+++ static/js/index.js
@@ -1,107 +1,82 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
+var INTERP_BASE = "./static/interpolation";
+var NUM_INTERP_FRAMES = 240;
 
+var hashTableImages = {};
+function preloadHashTableImages() {
+    const targets = ["00", "01", "02", "03", "04"];
+    targets_hash = {};
+    targets_hash["0"] = "00";
+    targets_hash["1"] = "01";
+    targets_hash["2"] = "02";
+    targets_hash["3"] = "03";
+    targets_hash["4"] = "04";
+    var NUM_INTER = 5;
 
-function magnify(imgID, zoom) {
-  var img, glass, w, h, bw;
-  img = document.getElementById(imgID);
-  console.log('before');
-  setTimeout(function(){
-      console.log('after');
-  },500);
-  console.log('lalalalalla');
-  /*create magnifier glass:*/
-  glass = document.createElement("DIV");
-  glass.setAttribute("class", "img-magnifier-glass");
-  /*insert magnifier glass:*/
-  img.parentElement.insertBefore(glass, img);
-  /*set background properties for the magnifier glass:*/
-  glass.style.backgroundImage = "url('" + img.src + "')";
-  glass.style.backgroundRepeat = "no-repeat";
-  glass.style.backgroundSize = (img.width * zoom ) + "px " + (img.height * zoom ) + "px";
-  bw = 3;
-  console.log(glass, glass.offsetWidth , glass.offsetHeight);
-  w = glass.offsetWidth / 2;
-  h = glass.offsetHeight / 2;
-  /*execute a function when someone moves the magnifier glass over the image:*/
-  glass.addEventListener("mousemove", moveMagnifier);
-  img.addEventListener("mousemove", moveMagnifier);
-  /*and also for touch screens:*/
-  glass.addEventListener("touchmove", moveMagnifier);
-  img.addEventListener("touchmove", moveMagnifier);
-
-  function moveMagnifier(e) {
-    var pos, x, y;
-    console.log('inside moveMagnifier')
-    /*prevent any other actions that may occur when moving over the image*/
-    e.preventDefault();
-    /*get the cursor's x and y positions:*/
-    pos = getCursorPos(e);
-    x = pos.x;
-    y = pos.y;
-    console.log("lalalalla" , e, x, y);
-    /*prevent the magnifier glass from being positioned outside the image:*/
-    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
-    if (x < w / zoom) {x = w / zoom;}
-    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
-    if (y < h / zoom) {y = h / zoom;}
-    /*set the position of the magnifier glass:*/
-    console.log("x,y,w,h: ",x,y,w,h)
-    glass.style.left = (x - w) + "px";
-    glass.style.top = (y ) + "px"; //(y -h) + "px";
-    console.log("!!!!!  ",glass.style.left , glass.style.top );
-    /*display what the magnifier glass "sees":*/
-    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px"; // '-' for negtive
-    console.log(glass, glass.style.backgroundPosition )
-  }
-
-  function getCursorPos(e) {
-    var a, x = 0, y = 0;
-    e = e || window.event;
-    /*get the x and y positions of the image:*/
-    a = img.getBoundingClientRect();
-    /*calculate the cursor's x and y coordinates, relative to the image:*/
-    x = e.pageX - a.left;
-    y = e.pageY - a.top;
-    /*consider any page scrolling:*/
-    x = x - window.pageXOffset;
-    y = y - window.pageYOffset;
-    return {x : x, y : y};
-  }
-}
-
-
-
-
-
-var INTERP_BASE = "./static/images/interpolate/seed"//"https:/xxx/interpolation/stacked";
-var NUM_INTERP_FRAMES = 98;
-// var INTERP_BASE = "https://homes.cs.washington.edu/~kpar/nerfies/interpolation/stacked";
-
-
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 1; i < NUM_INTERP_FRAMES; i++) {
-    if (i == 35 || i == 43)
-      i++;
-    else{
-      var path = INTERP_BASE + String(i).padStart(4, '0') + '.png';
-    // var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-      interp_images[i] = new Image();
-      interp_images[i].src = path;
+    let key;
+    let imageSrc;
+    for (let beta1 of targets) {
+        for (let beta2 of targets) {
+            for (let beta3 of targets) {
+                for (let beta4 of targets) {
+                    key = beta1 + "_" + beta2 + "_" + beta3 + "_" + beta4
+                    imageSrc = "./static/images/interpolation/" + key + ".png"
+                    hashTableImages[key] = new Image();
+                    hashTableImages[key].src = imageSrc
+                }
+            }
+        }
     }
-  }
 }
 
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
+function computeBlendWeightColor(beta) {
+    positiveColor = $.Color("#E06666");
+    negativeColor = $.Color("#0095dd");
+
+    if (beta < 0) {
+        blendColor = negativeColor;
+    } else {
+        blendColor = positiveColor;
+    }
+    white = $.Color("#FFF");
+    alpha = Math.abs(beta) / 0.75;
+
+    blendedColor = blendColor.alpha(alpha).blend(white.alpha(1 - alpha));
+
+    return blendedColor;
 }
+
+function updateHashTableImage() {
+    var beta1 = $("#slider-hash-table-1").val();
+    var beta2 = $("#slider-hash-table-2").val();
+    var beta3 = $("#slider-hash-table-3").val();
+    var beta4 = $("#slider-hash-table-4").val();
+
+    beta1 = targets_hash[beta1];
+    beta2 = targets_hash[beta2];
+    beta3 = targets_hash[beta3];
+    beta4 = targets_hash[beta4];
+
+    key = beta1 + "_" + beta2 + "_" + beta3 + "_" + beta4;
+
+    var image = hashTableImages[key];
+    image.ondragstart = function() { return false; };
+    image.oncontextmenu = function() { return false; };
+    $('#hash-table-image-wrapper').empty().append(image);
+
+    // Update vector
+//    $("#blend-weights-vector-1").css({"backgroundColor": computeBlendWeightColor(beta1)});
+//    $("#blend-weights-vector-2").css({"backgroundColor": computeBlendWeightColor(beta2)});
+//    $("#blend-weights-vector-3").css({"backgroundColor": computeBlendWeightColor(beta3)});
+//    $("#blend-weights-vector-4").css({"backgroundColor": computeBlendWeightColor(beta4)});
+}
+
 
 
 $(document).ready(function() {
+
+
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function() {
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
@@ -112,10 +87,10 @@ $(document).ready(function() {
 
     var options = {
 			slidesToScroll: 1,
-			slidesToShow: 4,
+			slidesToShow: 3,
 			loop: true,
 			infinite: true,
-			autoplay: true,
+			autoplay: false,
 			autoplaySpeed: 3000,
     }
 
@@ -139,44 +114,21 @@ $(document).ready(function() {
     	});
     }
 
-    // For interpolations
+
+    preloadHashTableImages();
+    updateHashTableImage();
+    $("#slider-hash-table-1, #slider-hash-table-2, #slider-hash-table-3, #slider-hash-table-4").on("input", function () {
+        updateHashTableImage();
+    });
+
     preloadInterpolationImages();
+
     $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);});
-    setInterpolationImage(50);
+      setInterpolationImage(this.value);
+    });
+    setInterpolationImage(0);
     $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
-  
     bulmaSlider.attach();
 
 })
-
-
-
-// large slider
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
---
+++ static/js/index_2.js
@@ -0,0 +1,172 @@
+ window.HELP_IMPROVE_VIDEOJS = false;
+//
+// var INTERP_BASE = "./static/interpolation/stacked";
+// var NUM_INTERP_FRAMES = 240;
+//
+// var interp_images = [];
+// function preloadInterpolationImages() {
+//   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
+//     var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+//     interp_images[i] = new Image();
+//     interp_images[i].src = path;
+//   }
+// }
+//
+// function setInterpolationImage(i) {
+//   var image = interp_images[i];
+//   image.ondragstart = function() { return false; };
+//   image.oncontextmenu = function() { return false; };
+//   $('#interpolation-image-wrapper').empty().append(image);
+// }
+
+var hashTableImages = {};
+function preloadHashTableImages() {
+    const targets = ["0.0", 0.25, 0.5, 0.75, 1.0];
+
+    let key;
+    let imageSrc;
+    for (let beta1 of targets) {
+        for (let beta2 of targets) {
+            for (let beta3 of targets) {
+                for (let beta4 of targets) {
+                    key = beta1 + "_" + beta2 + "_" + beta3 + "_" + beta4
+                    imageSrc = "./static/images/interpolations/" + key + ".png"
+                    hashTableImages[key] = new Image();
+                    hashTableImages[key].src = imageSrc
+                }
+            }
+        }
+    }
+}
+
+function computeBlendWeightColor(beta) {
+    positiveColor = $.Color("#E06666");
+    negativeColor = $.Color("#0095dd");
+
+    if (beta < 0) {
+        blendColor = negativeColor;
+    } else {
+        blendColor = positiveColor;
+    }
+    white = $.Color("#FFF");
+    alpha = Math.abs(beta) / 0.75;
+
+    blendedColor = blendColor.alpha(alpha).blend(white.alpha(1 - alpha));
+
+    return blendedColor;
+}
+
+function updateHashTableImage() {
+    var beta1 = $("#slider-hash-table-1").val();
+    var beta2 = $("#slider-hash-table-2").val();
+    var beta3 = $("#slider-hash-table-3").val();
+    var beta4 = $("#slider-hash-table-4").val();
+
+    beta1 = beta1 == 0 ? "0.0" : beta1;
+    beta2 = beta2 == 0 ? "0.0" : beta2;
+    beta3 = beta3 == 0 ? "0.0" : beta3;
+    beta4 = beta4 == 0 ? "0.0" : beta4;
+
+    key = beta1 + "_" + beta2 + "_" + beta3 + "_" + beta4;
+
+    var image = hashTableImages[key];
+    image.ondragstart = function() { return false; };
+    image.oncontextmenu = function() { return false; };
+    $('#hash-table-image-wrapper').empty().append(image);
+
+    // Update vector
+    $("#blend-weights-vector-1").css({"backgroundColor": computeBlendWeightColor(beta1)});
+    $("#blend-weights-vector-2").css({"backgroundColor": computeBlendWeightColor(beta2)});
+    $("#blend-weights-vector-3").css({"backgroundColor": computeBlendWeightColor(beta3)});
+    $("#blend-weights-vector-4").css({"backgroundColor": computeBlendWeightColor(beta4)});
+}
+
+
+$(document).ready(function() {
+    preloadHashTableImages();
+    updateHashTableImage();
+
+    var carousels = bulmaCarousel.attach('#results-carousel', {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 8000,
+    });
+
+    // Start playing next video in carousel and pause previous video to limit load on browser
+    for(var i = 0; i < carousels.length; i++) {
+        // Add listener to  event
+        carousels[i].on('before:show', state => {
+            var nextId = (state.next + state.length) % state.length;  // state.next can be -1 or larger than the number of videos
+            var nextVideoElement = $("#results-carousel .slider-item[data-slider-index='" + nextId + "'] video")[0];
+            var previousVideoElement = $("#results-carousel .slider-item[data-slider-index='" + state.index + "'] video")[0];
+
+            previousVideoElement.pause();
+            previousVideoElement.currentTime = 0;
+            nextVideoElement.currentTime = 0;
+            nextVideoElement.play();
+        });
+    }
+
+
+    $("#slider-hash-table-1, #slider-hash-table-2, #slider-hash-table-3").on("input", function () {
+        updateHashTableImage();
+    });
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+    });
+
+    var options = {
+			slidesToScroll: 1,
+			slidesToShow: 3,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
+    }
+
+		// Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.carousel', options);
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+    	// Add listener to  event
+    	carousels[i].on('before:show', state => {
+    		console.log(state);
+    	});
+    }
+
+    // Access to bulmaCarousel instance of an element
+    var element = document.querySelector('#my-element');
+    if (element && element.bulmaCarousel) {
+    	// bulmaCarousel instance is available as element.bulmaCarousel
+    	element.bulmaCarousel.on('before-show', function(state) {
+    		console.log(state);
+    	});
+    }
+
+    /*var player = document.getElementById('interpolation-video');
+    player.addEventListener('loadedmetadata', function() {
+      $('#interpolation-slider').on('input', function(event) {
+        console.log(this.value, player.duration);
+        player.currentTime = player.duration / 100 * this.value;
+      })
+    }, false);*/
+    preloadInterpolationImages();
+
+    $('#interpolation-slider').on('input', function(event) {
+      setInterpolationImage(this.value);
+    });
+    setInterpolationImage(0);
+    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+
+    bulmaSlider.attach();
+
+})
